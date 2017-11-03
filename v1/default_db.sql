772e2009379ce30e6ac5d481504b6c8710a9c52e
Ian Foose <ianfoose@me.com>
2017-11-03 14:44:23 -0400

    Update and rename database.sql to default_db.sql

---
+++ v1/default_db.sql
@@ -0,0 +1,64 @@
+CREATE TABLE `email_template_edits` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) DEFAULT NULL,
+  `body` text,
+  `date` datetime DEFAULT CURRENT_TIMESTAMP,
+  `name` varchar(45) DEFAULT NULL,
+  `template_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
+
+CREATE TABLE `email_templates` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `deleted` tinyint(1) DEFAULT '0',
+  `date` datetime DEFAULT CURRENT_TIMESTAMP,
+  `user_id` int(11) DEFAULT NULL,
+  `body` text,
+  `name` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=26 DEFAULT CHARSET=utf8;
+
+CREATE TABLE `errors_tbl` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `code` varchar(455) DEFAULT NULL,
+  `date` datetime DEFAULT CURRENT_TIMESTAMP,
+  `type` varchar(455) DEFAULT NULL,
+  `description` varchar(455) DEFAULT NULL,
+  `message` varchar(455) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=470 DEFAULT CHARSET=utf8;
+
+CREATE TABLE `mailing_list` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `email` varchar(255) DEFAULT NULL,
+  `subscriber` tinyint(1) DEFAULT '0',
+  `date` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+CREATE TABLE `push_tokens` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `token` varchar(155) DEFAULT NULL,
+  `deleted` tinyint(1) DEFAULT '0',
+  `date` datetime DEFAULT CURRENT_TIMESTAMP,
+  `user_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `tokens` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `u_id` int(11) DEFAULT NULL,
+  `date` datetime DEFAULT CURRENT_TIMESTAMP,
+  `exp_date` datetime DEFAULT NULL,
+  `revoked` tinyint(4) DEFAULT '0',
+  `token` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=186 DEFAULT CHARSET=utf8;
+
+CREATE TABLE `visitor_logs` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `date` datetime DEFAULT CURRENT_TIMESTAMP,
+  `ip` varchar(45) DEFAULT NULL,
+  `client` varchar(500) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1764 DEFAULT CHARSET=utf8;
