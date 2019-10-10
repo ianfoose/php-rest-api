e82d1b91161584692b2b830b4681969c3416d589
Ian Foose <ianfoose@me.com>
2019-10-10 17:36:36 -0400

    updates for email_subscriptions schema.

--- v1/sql/updates.sql
+++ v1/sql/updates.sql
@@ -1,2 +1,7 @@
 -- Audit logs
 ALTER TABLE `audit_logs` ADD COLUMN `row_id` int(11) DEFAULT NULL AFTER `object_id`;
+
+-- Email Subscriptions
+
+ALTER TABLE `email_subscriptions` ADD COLUMN `group` varchar(45) DEFAULT 'Default' AFTER `date`;
+UPDATE TABLE `email_subscriptions` SET group='Default';
